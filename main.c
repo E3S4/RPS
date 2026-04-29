99dbf8377d20aeb5ce1515599604b957794321d5
E3S4 <eesak490@gmail.com>
2026-04-29 14:10:21 +0530

    Tried adding rand for commputer choice it worked ngl fine

--- main.c
+++ main.c
@@ -24,27 +24,7 @@ int get_computer_choice() {
     return rand() % 3 + 1; // generates a number between 1 and 3
 }
 
-// determine the winner of a round
-int determine_winner(int player_choice, int computer_choice) {
-    if (player_choice == computer_choice) {
-        return 0; // tie
-    } else if ((player_choice == 1 && computer_choice == 3) ||
-                (player_choice == 2 && computer_choice == 1) ||
-                (player_choice == 3 && computer_choice == 2)) 
-                  return 1; // player wins
-    return -1; // computer wins
-}
-// function to store scores and display final results
-void display_final_results(int player_score, int computer_score) {
-    printf("Final Scores - Player: %d, Computer: %d\n", player_score, computer_score);
-    if (player_score > computer_score) {
-        printf("Congratulations! You win!\n");
-    } else if (player_score < computer_score) {
-        printf("Computer wins! Better luck next time.\n");
-    } else {
-        printf("It's a tie!\n");
-    }
-}
+
 
 // main function
 int main()
